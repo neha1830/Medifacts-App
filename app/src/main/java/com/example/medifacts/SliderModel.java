f3928a75c11715fc61d4f8c08b345897e02828c3
Neha Jha <jhaneha1018@gmail.com>
2020-11-27 01:15:59 +0530

    Updated SliderModel.java

---
+++ app/src/main/java/com/example/medifacts/SliderModel.java
@@ -0,0 +1,14 @@
+package com.example.medifacts;
+
+public class SliderModel {
+    private int banner;
+
+    public SliderModel(int banner) {
+        this.banner = banner;
+    }
+
+    public int getBanner() {
+        return banner;
+    }
+
+}
