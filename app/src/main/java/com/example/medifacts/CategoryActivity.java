3eaf708d00d6583d1401997c89453c5ebbf2427c
Neha Jha <jhaneha1018@gmail.com>
2020-12-03 19:01:23 +0530

    Added Categories

--- app/src/main/java/com/example/medifacts/CategoryActivity.java
+++ app/src/main/java/com/example/medifacts/CategoryActivity.java
@@ -1,78 +1,37 @@
-package com.example.medifacts.ui.home;
-
-import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
+package com.example.medifacts;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.MarginPageTransformer;
-import androidx.viewpager2.widget.ViewPager2;
 
-import com.example.medifacts.CategoryAdapter;
-import com.example.medifacts.CategoryModel;
-import com.example.medifacts.GridProductLayoutAdapter;
-import com.example.medifacts.HorizontalProductScrollAdapter;
-import com.example.medifacts.HorizontalProductScrollModel;
-import com.example.medifacts.R;
-import com.example.medifacts.SliderAdapter;
-import com.example.medifacts.SliderModel;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.medifacts.ui.home.HomePageAdapter;
+import com.example.medifacts.ui.home.HomeViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class HomeFragment extends Fragment {
+public class CategoryActivity extends AppCompatActivity {
 
-    public HomeFragment(){
-
-    }
-
-
-    private HomeViewModel homeViewModel;
     private RecyclerView categoryRecyclerView;
-    private CategoryAdapter categoryAdapter;
-    private RecyclerView testing;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        String title = getIntent().getStringExtra("CategoryName");
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-    @SuppressLint("ClickableViewAccessibility")
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        categoryRecyclerView = view.findViewById(R.id.category_recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        categoryRecyclerView.setLayoutManager(layoutManager);
-
-        final List<CategoryModel> categoryModelList = new ArrayList<CategoryModel>();
-        categoryModelList.add(new CategoryModel("link","Home"));
-        categoryModelList.add(new CategoryModel("link","Devices"));
-        categoryModelList.add(new CategoryModel("link","Covid Essentials"));
-        categoryModelList.add(new CategoryModel("link","Fitness"));
-        categoryModelList.add(new CategoryModel("link","Ayush"));
-        categoryModelList.add(new CategoryModel("link","Mom's Care"));
-        categoryModelList.add(new CategoryModel("link","Upload Prescription"));
-        categoryModelList.add(new CategoryModel("link","Consult Doctor"));
-        categoryModelList.add(new CategoryModel("link","Lab Tests"));
-
-        categoryAdapter = new CategoryAdapter(categoryModelList);
-
-        categoryRecyclerView.setAdapter(categoryAdapter);
-        categoryAdapter.notifyDataSetChanged();
+        categoryRecyclerView = findViewById(R.id.category_recyclerview);
 
         ////Banner Slider
         List<SliderModel> sliderModelList = new ArrayList<SliderModel>();
@@ -97,8 +56,6 @@ public class HomeFragment extends Fragment {
 
         //horizontalProduct Layout
 
-
-
         List<HorizontalProductScrollModel> horizontalProductScrollModelList = new ArrayList<>();
         horizontalProductScrollModelList.add(new HorizontalProductScrollModel(R.mipmap.apple,"Apple Cider Vinegar","100% Natural Vinegar 500ml","450.00"));
         horizontalProductScrollModelList.add(new HorizontalProductScrollModel(R.mipmap.green_email,"Apple Cider Vinegar","100% Natural Vinegar 500ml","450.00"));
@@ -114,29 +71,41 @@ public class HomeFragment extends Fragment {
         //horizontalProduct Layout
 
         ///////////////////
-        testing = view.findViewById(R.id.home_page_recycler_view);
-        LinearLayoutManager testingLayoutManager = new LinearLayoutManager(getContext());
+        LinearLayoutManager testingLayoutManager = new LinearLayoutManager(this);
         testingLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        testing.setLayoutManager(testingLayoutManager);
+        categoryRecyclerView.setLayoutManager(testingLayoutManager);
 
         List<HomeViewModel> homeViewModelList = new ArrayList<>();
         homeViewModelList.add(new HomeViewModel(0,sliderModelList));
         homeViewModelList.add(new HomeViewModel(1,R.mipmap.upload_prescription,"#ff0000"));
         homeViewModelList.add(new HomeViewModel(2,"Deal of the Day",horizontalProductScrollModelList));
-        homeViewModelList.add(new HomeViewModel(0,sliderModelList));
         homeViewModelList.add(new HomeViewModel(1,R.mipmap.upload_prescription,"#ffffff"));
         homeViewModelList.add(new HomeViewModel(2,"Deal of the Day",horizontalProductScrollModelList));
         homeViewModelList.add(new HomeViewModel(3,"Deal of the Day",horizontalProductScrollModelList));
-        homeViewModelList.add(new HomeViewModel(0,sliderModelList));
         homeViewModelList.add(new HomeViewModel(1,R.mipmap.upload_prescription,"#fff000"));
+        homeViewModelList.add(new HomeViewModel(3,"Deal of the Day",horizontalProductScrollModelList));
 
 
         HomePageAdapter adapter = new HomePageAdapter(homeViewModelList);
-        testing.setAdapter(adapter);
+        categoryRecyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+    }
 
-        /////////////////////
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_icon, menu);
+        return true;
+    }
 
-        return view;
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.main_search_icon) {
+            return true;
+        }else if (id == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
