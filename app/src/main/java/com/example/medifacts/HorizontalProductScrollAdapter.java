9267cdb91c9177bfc726d260f7e7357e240c0f83
Neha Jha <jhaneha1018@gmail.com>
2020-12-03 23:18:53 +0530

    Added ProductDetailsActivity

--- app/src/main/java/com/example/medifacts/HorizontalProductScrollAdapter.java
+++ app/src/main/java/com/example/medifacts/HorizontalProductScrollAdapter.java
@@ -1,5 +1,6 @@
 package com.example.medifacts;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -53,12 +54,20 @@ public class HorizontalProductScrollAdapter extends RecyclerView.Adapter<Horizon
         private TextView productTitle;
         private TextView productDescription;
         private TextView productPrice;
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull final View itemView) {
             super(itemView);
             productImage = itemView.findViewById(R.id.hs_product_image);
             productTitle = itemView.findViewById(R.id.hs_product_title);
             productDescription = itemView.findViewById(R.id.hs_product_description);
             productPrice = itemView.findViewById(R.id.hs_product_price);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent productDetailsIntent = new Intent(itemView.getContext(),ProductDetailsActivity.class);
+                    itemView.getContext().startActivity(productDetailsIntent);
+                }
+            });
         }
 
         private void setProductImage(int resource){
